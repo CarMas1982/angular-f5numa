73c307a8c855c1999a3890d97e52df25ce93feb1
CarMas1982 <carmine.massei@outlook.it>
2019-10-01 17:09:39 +0200

    3 files modified, 1 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -9,6 +10,7 @@ import { Input } from '@angular/core';
 export class ProductAlertsComponent implements OnInit {
   // The @Input() decorator indicates that the property value passes in from the component's parent, the product list component.
   @Input() product;
+  @Output() notify = new EventEmitter();
 
   constructor() { }
 
